810f49508d0450c2d0bd226398e1e23a3d5e64bb
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-02-12 12:41:53 +0100

    Add old math code to project

---
+++ source/premake5.lua
@@ -0,0 +1,39 @@
+require 'settings'
+
+--premake5.lua
+ 
+solution (SOLUTION_NAME)
+	configurations { "Debug", "Release" }
+	
+	platforms {
+		"Win32",
+		"Win64"
+	}
+	
+	filter { "platforms:Win32" }
+		system "Windows"
+		architecture "x32"
+		
+	filter { "platforms:Win64" }
+		system "Windows"
+		architecture "x64"
+		
+	configuration { "Debug" }
+		defines { "DEBUG" }
+		flags { "Symbols" }
+		
+	configuration { "Release" }
+		defines { "RELEASE", "NDEBUG" }
+		optimize "On"
+	 
+project (PROJECT_NAME)
+	kind "StaticLib"
+	language "C++"
+	
+	objdir ("$(SolutionDir)../build/")
+	
+	files { "**.h", "**.cpp", "**.hpp" }
+	
+	targetdir "$(SolutionDir)../lib/"
+	debugdir "$(SolutionDir)../lib/"
+	targetname( PROJECT_NAME .. " - $(Configuration)" )
