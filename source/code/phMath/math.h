32206443e88aebc35d0ef45b4ccb7d0aa7bbd4f2
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-18 01:59:57 +0200

    Update formatting in math.h

--- source/code/phMath/math.h
+++ source/code/phMath/math.h
@@ -5,31 +5,30 @@ namespace std
 #include <math.h>
 }
 
-
 namespace pm
 {
 
 // Returns the square root of the specified value.
-inline float sqrt( const float& value )
+inline float sqrt(const float& value)
 {
-	return static_cast<float>( std::sqrt( value ) );
+	return static_cast<float>(std::sqrt(value));
 }
 
 
 
 // Returns pow of the specified base
-inline float pow( const float& base, const float& exponent )
+inline float pow(const float& base, const float& exponent)
 {
-	return static_cast<float>( std::pow( base, exponent ) );
+	return static_cast<float>(std::pow(base, exponent));
 }
 
 
 
 // Calculates and returns the absolute 
 // value of the specified float.
-inline float abs( const float& value )
+inline float abs(const float& value)
 {
-	if( value < 0.f )
+	if (value < 0.f)
 		return -value;
 
 	return value;
@@ -39,11 +38,11 @@ inline float abs( const float& value )
 
 // Clamps the specified value between the specified 
 // min and max values, and returns the result.
-inline float clamp( const float& value, const float& min, const float& max )
+inline float clamp(const float& value, const float& min, const float& max)
 {
-	if( value <= min )
+	if (value <= min)
 		return min;
-	if( value >= max )
+	if (value >= max)
 		return max;
 
 	return value;
@@ -53,11 +52,11 @@ inline float clamp( const float& value, const float& min, const float& max )
 
 // Clamps the specified value between 0 and 1 
 // and returns the result.
-inline float saturate( float value )
+inline float saturate(float value)
 {
-	if( value <= 0.f )
+	if (value <= 0.f)
 		return 0.f;
-	if( value >= 1.f )
+	if (value >= 1.f)
 		return 1.f;
 
 	return value;
@@ -68,21 +67,21 @@ inline float saturate( float value )
 // Returns -1 if value is less than 0
 // Returns 0 if value is 0
 // Returns 1 if value is greater than 0
-inline float sign( const float& value )
+inline float sign(const float& value)
 {
-	if( value == 0.f )
+	if (value == 0.f)
 		return 0.f;
-	if( value > 0.f )
+	if (value > 0.f)
 		return 1.f;
 
 	return -1.f;
 }
 
-inline int sign( const int& value )
+inline int sign(const int& value)
 {
-	if( value == 0 )
+	if (value == 0)
 		return 0;
-	if( value > 0 )
+	if (value > 0)
 		return 1;
 	return -1;
 }
@@ -91,7 +90,7 @@ inline int sign( const int& value )
 
 // Converts degrees to radians.
 // Uses only multiplication with const value to optimize performance.
-inline float toRadians( const float& degrees )
+inline float toRadians(const float& degrees)
 {
 	return degrees * 0.0174532925f;
 }
@@ -100,7 +99,7 @@ inline float toRadians( const float& degrees )
 
 // Converts radians to degrees.
 // Uses only multiplication with const value to optimize performance.
-inline float toDegrees( const float& radians )
+inline float toDegrees(const float& radians)
 {
 	return radians * 57.2957795f;
 }
@@ -108,64 +107,64 @@ inline float toDegrees( const float& radians )
 
 
 // Returns sine from degrees.
-inline float sin( const float& degrees )
+inline float sin(const float& degrees)
 {
-	return static_cast<float>( std::sin( toRadians( degrees ) ) );
+	return static_cast<float>(std::sin(toRadians(degrees)));
 }
 
 
 
 // Returns sine from radians.
-inline float sinRad( const float& radians )
+inline float sinRad(const float& radians)
 {
-	return static_cast<float>( std::sin( radians ) );
+	return static_cast<float>(std::sin(radians));
 }
 
 
 
 // Returns asin
-inline float asin( const float& sinValue )
+inline float asin(const float& sinValue)
 {
-	return toDegrees( static_cast<float>( std::asin( sinValue ) ) );
+	return toDegrees(static_cast<float>(std::asin(sinValue)));
 }
 
 
 
 // Returns cosine from degrees.
-inline float cos( const float& degrees )
+inline float cos(const float& degrees)
 {
-	return static_cast<float>( std::cos( toRadians( degrees ) ) );
+	return static_cast<float>(std::cos(toRadians(degrees)));
 }
 
 
 
 // Returns cosine from radians.
-inline float cosRad( const float& radians )
+inline float cosRad(const float& radians)
 {
-	return static_cast<float>( std::cos( radians ) );
+	return static_cast<float>(std::cos(radians));
 }
 
 
 
 // Returns acos
-inline float acos( const float& cosValue )
+inline float acos(const float& cosValue)
 {
-	return toDegrees( static_cast<float>( std::acos( cosValue ) ) );
+	return toDegrees(static_cast<float>(std::acos(cosValue)));
 }
 
 
 
 // Returns tangent from degrees.
-inline float tan( const float& degrees )
+inline float tan(const float& degrees)
 {
-	return static_cast<float>( std::tan( toRadians( degrees ) ) );
+	return static_cast<float>(std::tan(toRadians(degrees)));
 }
 
 
 // Returns atan
-inline float atan( const float& tanValue )
+inline float atan(const float& tanValue)
 {
-	return toDegrees( static_cast<float>( std::atan( tanValue ) ) );
+	return toDegrees(static_cast<float>(std::atan(tanValue)));
 }
 
 
@@ -173,41 +172,41 @@ inline float atan( const float& tanValue )
 // Interpolates between two values using a factor.
 // factor 0 is no interpolation and factor 1 is full.
 // The interpolation can go outside range, both positive and negative.
-inline float lerp( const float& start, const float& end, const float& factor )
+inline float lerp(const float& start, const float& end, const float& factor)
 {
-	return start + ( end - start ) * factor;
+	return start + (end - start) * factor;
 }
 
 
 
 // Cosine interpolates between the two given values using the factor.
-inline float interpolateCosine( const float& start, const float& end, const float& factor )
+inline float interpolateCosine(const float& start, const float& end, const float& factor)
 {
-	return start + ( end - start ) * ( ( -cos( factor * 180.0f ) + 1.0f ) * 0.5f );
+	return start + (end - start) * ((-cos(factor * 180.0f) + 1.0f) * 0.5f);
 }
 
 
 
 // Rounds value to nearest whole number
-inline float roundf( const float& value )
+inline float roundf(const float& value)
 {
-	return static_cast<float>( static_cast<int>( value + 0.5f ) );
+	return static_cast<float>(static_cast<int>(value + 0.5f));
 }
 
 
 
 // Rounds value downwards
-inline float floorf( const float& value )
+inline float floorf(const float& value)
 {
-	return static_cast<float>( static_cast<int>( value + 1.0f ) );
+	return static_cast<float>(static_cast<int>(value + 1.0f));
 }
 
 
 
 // Rounds value upwards
-inline float ceilf( const float& value )
+inline float ceilf(const float& value)
 {
-	return static_cast<float>( static_cast<int>( value ) );
+	return static_cast<float>(static_cast<int>(value));
 }
 
 }
