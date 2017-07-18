24bd9613ea649ba35adfe81e1e9866968d4c5561
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-18 02:07:35 +0200

    Format matrix class

--- source/code/phMath/mat4.cpp
+++ source/code/phMath/mat4.cpp
@@ -22,14 +22,14 @@ namespace pm
 mat4::mat4()
 {
 	// Set all elements to 0
-	memset( elements, 0, 16 * sizeof( float ) );
+	memset(elements, 0, 16 * sizeof(float));
 }
 
 
 
-mat4::mat4( const mat4& other )
+mat4::mat4(const mat4& other)
 {
-	for( unsigned char i = 0; i < 16; i++ )
+	for (unsigned char i = 0; i < 16; i++)
 	{
 		elements[i] = other.elements[i];
 	}
@@ -37,13 +37,13 @@ mat4::mat4( const mat4& other )
 
 
 
-mat4::mat4( const float diagonal )
+mat4::mat4(const float diagonal)
 {
 	// Set all elements to 0
-	memset( elements, 0, 16 * sizeof( float ) );
+	memset(elements, 0, 16 * sizeof(float));
 
 	// Set the diagonal
-	for( unsigned char i = 0; i < 4; ++i )
+	for (unsigned char i = 0; i < 4; ++i)
 	{
 		elements[i + i * 4] = diagonal;
 	}
@@ -57,26 +57,26 @@ mat4::~mat4()
 
 
 
-void mat4::Translate( const vec3& translation, EMultiplyOrder Order )
+void mat4::translate(const vec3& translation, EMultiplyOrder Order)
 {
 	mat4 Result(1.0f);
 	Result.translation = translation;
 
-	MULTIPLY_THIS( Order, Result );
+	MULTIPLY_THIS(Order, Result);
 }
 
 
-void mat4::RotateZYX( const vec3& axis, EMultiplyOrder Order )
+void mat4::rotateZYX(const vec3& axis, EMultiplyOrder Order)
 {
-	float ca = cos( axis.x );
-	float sa = sin( axis.x );
-	float cb = cos( axis.y );
-	float sb = sin( axis.y );
-	float cc = cos( axis.z );
-	float sc = sin( axis.z );
+	float ca = cos(axis.x);
+	float sa = sin(axis.x);
+	float cb = cos(axis.y);
+	float sb = sin(axis.y);
+	float cc = cos(axis.z);
+	float sc = sin(axis.z);
 
 	// ZYX
-	mat4 result( 1.0f );
+	mat4 result(1.0f);
 	result.left.x = cc * cb;
 	result.left.y = sc * cb;
 	result.left.z = -sb;
@@ -89,19 +89,19 @@ void mat4::RotateZYX( const vec3& axis, EMultiplyOrder Order )
 	result.forward.y = -cc * sa + sc * sb * ca;
 	result.forward.z = cb * ca;
 
-	MULTIPLY_THIS( Order, result );
+	MULTIPLY_THIS(Order, result);
 }
 
-void mat4::RotateXYZ( const vec3& axis, EMultiplyOrder Order )
+void mat4::rotateXYZ(const vec3& axis, EMultiplyOrder Order)
 {
-	float ca = cos( axis.x );
-	float sa = sin( axis.x );
-	float cb = cos( axis.y );
-	float sb = sin( axis.y );
-	float cc = cos( axis.z );
-	float sc = sin( axis.z );
+	float ca = cos(axis.x);
+	float sa = sin(axis.x);
+	float cb = cos(axis.y);
+	float sb = sin(axis.y);
+	float cc = cos(axis.z);
+	float sc = sin(axis.z);
 
-	mat4 result( 1.0f );
+	mat4 result(1.0f);
 	result.left.x = cb * cc;
 	result.left.y = sa * sb * cc + ca * sc;
 	result.left.z = -ca * sb * cc + sa * sc;
@@ -114,11 +114,11 @@ void mat4::RotateXYZ( const vec3& axis, EMultiplyOrder Order )
 	result.forward.y = -sa * cb;
 	result.forward.z = ca * cb;
 
-	MULTIPLY_THIS( Order, result );
+	MULTIPLY_THIS(Order, result);
 }
 
 
-void mat4::Scale( const vec3& scale, EMultiplyOrder Order )
+void mat4::scale(const vec3& scale, EMultiplyOrder Order)
 {
 	elements[0 + 0 * 4] = scale.x;
 	elements[1 + 1 * 4] = scale.y;
@@ -127,12 +127,12 @@ void mat4::Scale( const vec3& scale, EMultiplyOrder Order )
 
 
 
-void mat4::Transpose()
+void mat4::transpose()
 {
-	mat4 result( 1 );
-	for( unsigned char c = 0; c < 4; ++c )
+	mat4 result(1);
+	for (unsigned char c = 0; c < 4; ++c)
 	{
-		for( unsigned char r = 0; r < 4; ++r )
+		for (unsigned char r = 0; r < 4; ++r)
 		{
 			result.elements[c + r * 4] = elements[r + c * 4];
 		}
@@ -143,9 +143,9 @@ void mat4::Transpose()
 
 
 
-void mat4::Orthonormalize()
+void mat4::orthonormalize()
 {
-	Orthogonalize();
+	orthogonalize();
 	left.normalize();
 	up.normalize();
 	forward.normalize();
@@ -153,17 +153,17 @@ void mat4::Orthonormalize()
 
 
 
-void mat4::Orthogonalize()
+void mat4::orthogonalize()
 {
-	left = forward.cross( up );
-	up = left.cross( forward );
+	left = forward.cross(up);
+	up = left.cross(forward);
 }
 
 
 
-mat4 mat4::GetViewMatrix() const
+mat4 mat4::getViewMatrix() const
 {
-	mat4 result( 1.0f );
+	mat4 result(1.0f);
 
 	result.left.x = left.x;
 	result.left.y = up.x;
@@ -177,16 +177,16 @@ mat4 mat4::GetViewMatrix() const
 	result.forward.y = up.z;
 	result.forward.z = -forward.z;
 
-	result.translation.x = -left.dot( translation );
-	result.translation.y = -up.dot( translation );
-	result.translation.z = forward.dot( translation );
+	result.translation.x = -left.dot(translation);
+	result.translation.y = -up.dot(translation);
+	result.translation.z = forward.dot(translation);
 
 	return result;
 }
 
 
 
-mat4 mat4::GetInverse() const
+mat4 mat4::getInverse() const
 {
 	float temp[16];
 
@@ -305,9 +305,9 @@ mat4 mat4::GetInverse() const
 	float determinant = elements[0] * temp[0] + elements[1] * temp[4] + elements[2] * temp[8] + elements[3] * temp[12];
 	determinant = 1.0f / determinant;
 
-	mat4 result( 0.0f );
+	mat4 result(0.0f);
 
-	for( unsigned char i = 0; i < 4 * 4; i++ )
+	for (unsigned char i = 0; i < 4 * 4; i++)
 		result.elements[i] = temp[i] * determinant;
 
 	return result;
@@ -315,7 +315,7 @@ mat4 mat4::GetInverse() const
 
 
 
-mat4 mat4::Orthographic( float left, float right, float bottom, float top, float near, float far )
+mat4 mat4::orthographic(float left, float right, float bottom, float top, float near, float far)
 {
 	mat4 result;
 
@@ -327,9 +327,9 @@ mat4 mat4::Orthographic( float left, float right, float bottom, float top, float
 	result.up.y = 2.0f / H;
 	result.forward.z = -2.0f / D;
 
-	result.translation.x = -( ( right + left ) / W );
-	result.translation.y = -( ( top + bottom ) / H );
-	result.translation.z = -( ( far + near ) / D );
+	result.translation.x = -((right + left) / W);
+	result.translation.y = -((top + bottom) / H);
+	result.translation.z = -((far + near) / D);
 	result.elements[15] = 1.0f;
 
 	return result;
@@ -337,31 +337,31 @@ mat4 mat4::Orthographic( float left, float right, float bottom, float top, float
 
 
 
-mat4 mat4::Perspective( float fov, float aspectRatio, float near, float far )
+mat4 mat4::perspective(float fov, float aspectRatio, float near, float far)
 {
-	mat4 result( 0.0f );
+	mat4 result(0.0f);
 
-	result.left.x			= ( 1.0f / tan( fov * 0.5f ) ) / aspectRatio;
-	result.up.y				= 1.0f / tan( fov * 0.5f );
-	result.forward.z		= ( near + far ) / ( near - far );
-	result.translation.z	= ( 2.0f * near * far ) / ( near - far );
-	result.elements[11]		= -1.0f;
+	result.left.x = (1.0f / tan(fov * 0.5f)) / aspectRatio;
+	result.up.y = 1.0f / tan(fov * 0.5f);
+	result.forward.z = (near + far) / (near - far);
+	result.translation.z = (2.0f * near * far) / (near - far);
+	result.elements[11] = -1.0f;
 
 	return result;
 }
 
 
 
-mat4 mat4::operator*( const mat4& other )
+mat4 mat4::operator*(const mat4& other)
 {
 	mat4 result;
 	float sum;
-	for( unsigned char row = 0; row < 4; ++row )
+	for (unsigned char row = 0; row < 4; ++row)
 	{
-		for( unsigned char col = 0; col < 4; ++col )
+		for (unsigned char col = 0; col < 4; ++col)
 		{
 			sum = 0.0f;
-			for( unsigned char i = 0; i < 4; ++i )
+			for (unsigned char i = 0; i < 4; ++i)
 			{
 				sum += elements[i + row * 4] * other.elements[col + i * 4];
 			}
@@ -374,7 +374,7 @@ mat4 mat4::operator*( const mat4& other )
 
 
 
-float& mat4::operator[]( unsigned char id )
+float& mat4::operator[](unsigned char id)
 {
 	return elements[id];
 }
--- source/code/phMath/mat4.h
+++ source/code/phMath/mat4.h
@@ -18,7 +18,7 @@ public:
 
 	// Sets all values to 0 as default
 	mat4();
-	mat4( const mat4& other );
+	mat4(const mat4& other);
 
 	// Defaults all values to 0, except the diagonal.
 	// ie. pmMat4( 1.0f ) creates an identity matrix.
@@ -27,7 +27,7 @@ public:
 	// [0 1 0 0]
 	// [0 0 1 0]
 	// [0 0 0 1]
-	mat4( const float diagonal );
+	mat4(const float diagonal);
 
 	~mat4();
 
@@ -40,43 +40,43 @@ public:
 		struct
 		{
 			// Instead of using padding floats, we align the vectors with 16 bytes
-			_declspec( align( 16 ) ) vec3 left;
-			_declspec( align( 16 ) ) vec3 up;
-			_declspec( align( 16 ) ) vec3 forward;
-			_declspec( align( 16 ) ) vec3 translation;
+			_declspec(align(16)) vec3 left;
+			_declspec(align(16)) vec3 up;
+			_declspec(align(16)) vec3 forward;
+			_declspec(align(16)) vec3 translation;
 		};
 	};
 
-	void Translate( const vec3& translation, EMultiplyOrder Order = COMBINE_POST_MULTIPLY );
-	void RotateZYX( const vec3& axis, EMultiplyOrder Order = COMBINE_POST_MULTIPLY ); // Recommended for camera rotation
-	void RotateXYZ( const vec3& axis, EMultiplyOrder Order = COMBINE_POST_MULTIPLY );
-	void Scale( const vec3& scale, EMultiplyOrder Order = COMBINE_POST_MULTIPLY );
-	void Transpose();
+	void translate(const vec3& translation, EMultiplyOrder Order = COMBINE_POST_MULTIPLY);
+	void rotateZYX(const vec3& axis, EMultiplyOrder Order = COMBINE_POST_MULTIPLY); // Recommended for camera rotation
+	void rotateXYZ(const vec3& axis, EMultiplyOrder Order = COMBINE_POST_MULTIPLY);
+	void scale(const vec3& scale, EMultiplyOrder Order = COMBINE_POST_MULTIPLY);
+	void transpose();
 
 	// Orthogonalizes this matrix, then normalizes it. 
-	void Orthonormalize();
+	void orthonormalize();
 
 	// Orthogonalizes this matrix using the following priority order:
 	// forward-up-left
-	void Orthogonalize();
+	void orthogonalize();
 
-	mat4 GetViewMatrix() const;
-	mat4 GetInverse() const;
+	mat4 getViewMatrix() const;
+	mat4 getInverse() const;
 
-	static mat4 Orthographic( float left, float right, float bottom, float top, float near, float far );
-	static mat4 Perspective( float fov, float aspectRatio, float near, float far );
+	static mat4 orthographic(float left, float right, float bottom, float top, float near, float far);
+	static mat4 perspective(float fov, float aspectRatio, float near, float far);
 
-	mat4 operator * ( const mat4& other );
+	mat4 operator * (const mat4& other);
 	float& operator [] (unsigned char id);
 
-	void* operator new( size_t i )
+	void* operator new(size_t i)
 	{
-		return _mm_malloc( i, 16 );
+		return _mm_malloc(i, 16);
 	}
 
-	void operator delete( void* p )
+	void operator delete(void* p)
 	{
-		_mm_free( p );
+		_mm_free(p);
 	}
 
 };
