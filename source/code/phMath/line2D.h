75c567ce6b978381ec1a9dfa8fff9ec941f414b4
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-18 02:10:55 +0200

    Reformat line classes

--- source/code/phMath/line2D.h
+++ source/code/phMath/line2D.h
@@ -9,35 +9,33 @@ namespace pm
 class line2D
 {
 public:
-
 	line2D()
 	{
 
 	}
 
-	line2D( const vec2& startPos, const vec2& endPos )
+	line2D(const vec2& startPos, const vec2& endPos)
 	{
 		this->startPos = startPos;
 		this->endPos = endPos;
 	}
 
-	inline const float GetLength( vec2* lengthVectorOut = nullptr )
+	inline const float getLength(vec2* lengthVectorOut = nullptr)
 	{
 		vec2 lengthVector = endPos - startPos;
-		if( lengthVectorOut )
+		if (lengthVectorOut)
 			*lengthVectorOut = lengthVector;
 
 		return lengthVector.length();
 	}
 
-	inline const vec2 GetPositionAtFraction( const float& fraction )
+	inline const vec2 getPositionAtFraction(const float& fraction)
 	{
-		return startPos + ( endPos - startPos ) * fraction;
+		return startPos + (endPos - startPos) * fraction;
 	}
 
 	vec2 startPos;
 	vec2 endPos;
-
 };
 
 }
--- source/code/phMath/line3D.h
+++ source/code/phMath/line3D.h
@@ -9,35 +9,33 @@ namespace pm
 class line3D
 {
 public:
-
 	line3D()
 	{
 
 	}
 
-	line3D( const vec3& startPos, const vec3& endPos )
+	line3D(const vec3& startPos, const vec3& endPos)
 	{
 		this->startPos = startPos;
 		this->endPos = endPos;
 	}
 
-	inline const float GetLength( vec3* lengthVectorOut = nullptr )
+	inline const float getLength(vec3* lengthVectorOut = nullptr)
 	{
 		vec3 lengthVector = endPos - startPos;
-		if( lengthVectorOut )
+		if (lengthVectorOut)
 			*lengthVectorOut = lengthVector;
 
 		return lengthVector.length();
 	}
 
-	inline const vec3 GetPositionAtFraction( const float& fraction )
+	inline const vec3 getPositionAtFraction(const float& fraction)
 	{
-		return startPos + ( endPos - startPos ) * fraction;
+		return startPos + (endPos - startPos) * fraction;
 	}
 
 	vec3 startPos;
 	vec3 endPos;
-
 };
 
 }
