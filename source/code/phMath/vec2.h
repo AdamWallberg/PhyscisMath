4e2375d16a72f6d59cd4f04e847f35ba0fa3b82e
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-07-18 02:13:56 +0200

    Reformat vec2 class

--- source/code/phMath/vec2.h
+++ source/code/phMath/vec2.h
@@ -12,8 +12,8 @@ class vec2
 public:
 
 	// Constructors / Destructor
-	vec2( const float& x = 0.0f, const float& y = 0.0f );
-	vec2( const vec2& rOther );
+	vec2(const float& x = 0.0f, const float& y = 0.0f);
+	vec2(const vec2& rOther);
 
 	~vec2();
 
@@ -38,12 +38,12 @@ public:
 
 	////////////////////////////////////////////////////////////////
 
-		// Methods
+	// Methods
 
-		// Performance heavy, use lengthSquared() when possible.
+	// Performance heavy, use lengthSquared() when possible.
 	inline const float length() const
 	{
-		return sqrt( lengthSquared() );
+		return sqrt(lengthSquared());
 
 	} // length
 
@@ -56,17 +56,17 @@ public:
 	// Returns a normalized version of this vector. 
 	// normalize() automatically calculates the length
 	// of the vector, which can be stored in a float pointer.
-	inline const vec2 normalize( float* pOutLength = nullptr ) const
+	inline const vec2 normalize(float* pOutLength = nullptr) const
 	{
 		vec2 out;
 		const float length = this->length();
 
 		// return length if pointer is given
-		if( pOutLength )
+		if (pOutLength)
 			*pOutLength = length;
 
 		// Don't divide by zero
-		if( length != 0.0f )
+		if (length != 0.0f)
 		{
 			out.x = x / length;
 			out.y = y / length;
@@ -77,7 +77,7 @@ public:
 	} // normalize
 
 	// Dot product methods
-	inline const float& dot( const vec2& rOther ) const
+	inline const float& dot(const vec2& rOther) const
 	{
 		return x * rOther.x + y * rOther.y;
 
@@ -86,40 +86,40 @@ public:
 	// String conversion
 	inline const std::string toString()
 	{
-		return "x: " + std::to_string( x ) + " y: " + std::to_string( y );
+		return "x: " + std::to_string(x) + " y: " + std::to_string(y);
 
 	} // toString
 
-////////////////////////////////////////////////////////////////
+		////////////////////////////////////////////////////////////////
 
-	// Addition operators
-	const vec2 operator + ( const vec2& in ) const;
-	const vec2 operator + ( const float in ) const;
-	void operator += ( const vec2& in );
-	void operator += ( const float in );
+		// Addition operators
+	const vec2 operator + (const vec2& in) const;
+	const vec2 operator + (const float in) const;
+	void operator += (const vec2& in);
+	void operator += (const float in);
 
 	// Subtraction operators
-	const vec2 operator - ( const vec2& in ) const;
-	const vec2 operator - ( const float in ) const;
+	const vec2 operator - (const vec2& in) const;
+	const vec2 operator - (const float in) const;
 	const vec2 operator - () const;
-	void operator -= ( const vec2& in );
-	void operator -= ( const float in );
+	void operator -= (const vec2& in);
+	void operator -= (const float in);
 
 	// Multiplication
-	const vec2 operator * ( const vec2& in ) const;
-	const vec2 operator * ( const float in ) const;
-	void operator *= ( const vec2& in );
-	void operator *= ( const float in );
+	const vec2 operator * (const vec2& in) const;
+	const vec2 operator * (const float in) const;
+	void operator *= (const vec2& in);
+	void operator *= (const float in);
 
 	// Division operators
-	const vec2 operator / ( const vec2& in ) const;
-	const vec2 operator / ( const float in ) const;
-	void operator /= ( const vec2& in );
-	void operator /= ( const float in );
+	const vec2 operator / (const vec2& in) const;
+	const vec2 operator / (const float in) const;
+	void operator /= (const vec2& in);
+	void operator /= (const float in);
 
 	// Comparison operators
-	bool operator == ( const vec2& in ) const;
-	bool operator != ( const vec2& in ) const;
+	bool operator == (const vec2& in) const;
+	bool operator != (const vec2& in) const;
 
 };
 
